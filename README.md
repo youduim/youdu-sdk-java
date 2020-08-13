179ddd1ae28dccbe9d4c1ef62c656666e97f8886
karl.hong <karl.hong@youdu.im>
2020-08-13 15:14:38 +0800

    下载zip部分移动到会话管理部分

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ SDK类介绍
 | SysMsgClient   | 系统消息发送
 | IdentifyClient | 单点登录身份认证
 | SessionClient  | 会话管理及会话消息发送
-| CustomMenuClient | 自定义菜单
 
 ***
 ## 目录
@@ -22,7 +21,6 @@ SDK类介绍
 * [SysMsgClient](#SysMsgClient)
 * [IdentifyClient](#IdentifyClient)
 * [SessionClient](#SessionClient)
-* [CustomMenuClient](#CustomMenuClient)
 ***
 
 ### YDApp 有度应用对象
@@ -307,8 +305,8 @@ SDK类介绍
     userId: 用户账号
 ***
 	
-	
-### SessionClient 会话管理
+
+### SessionClient
 --------------------
 #### 构建SessionClient对象
     import im.youdu.sdk.client;
@@ -443,8 +441,6 @@ SDK类介绍
     toUser: 消息接收者账号
     videoData: 视频文件二进制数据
 
-### CustomMenuClient
---------------------
 #### downloadChatMsgZipFile  获取文件的byte[]内容
     appClient.downloadChatMsgZipFile(String forwardId)
     forwardId:  转发业务ID
--- src/main/java/im/youdu/sdk/client/SessionClient.java
+++ src/main/java/im/youdu/sdk/client/SessionClient.java
@@ -10,7 +10,15 @@ import im.youdu.sdk.exception.FileIOException;
 import im.youdu.sdk.exception.HttpRequestException;
 import im.youdu.sdk.exception.ParamParserException;
 import im.youdu.sdk.util.Helper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -318,6 +326,75 @@ public class SessionClient {
 //----------------------------------------------------------------------------------------------------------------------
     private String uriSendMsg() throws ParamParserException, HttpRequestException, AESCryptoException {
         return String.format("%s%s%s?accessToken=%s", YdApi.SCHEME,this.host,YdApi.API_SESSION_SEND_MSG,this.tokenClient.getToken()) ;
+    }
+
+    /**
+     * 下载会话消息的zip文件并保存到dir目录
+     * @param forwardId 转发id
+     * @param dir 保存目录
+     * @return
+     * @throws IOException
+     * @throws HttpRequestException
+     * @throws FileIOException
+     * @throws AESCryptoException
+     * @throws ParamParserException
+     */
+    public FileInfo downloadChatMsgZipFileAndSave(String forwardId, String dir) throws IOException, HttpRequestException, FileIOException, AESCryptoException, ParamParserException {
+        byte[] bytes = this.downloadChatMsgZipFile(forwardId);
+
+        FileInfo fileInfo = new FileInfo();
+        fileInfo.setData(bytes);
+        fileInfo.setName(forwardId + ".zip");
+        fileInfo.setMediaId(forwardId);
+        fileInfo.setPath(Helper.saveFile(fileInfo, dir));
+
+        return fileInfo;
+    }
+
+    /**
+     * 下载会话消息的zip文件
+     *
+     * @param forwardId 转发Id
+     * @return
+     * @throws HttpRequestException
+     * @throws FileIOException
+     * @throws AESCryptoException
+     * @throws ParamParserException
+     */
+    public byte[] downloadChatMsgZipFile(String forwardId) throws HttpRequestException, FileIOException, AESCryptoException, ParamParserException {
+
+        JsonObject mediaIdJson = new JsonObject();
+        mediaIdJson.addProperty("forward_id", forwardId);
+        String cipherId = this.crypto.encrypt(Helper.utf8Bytes(mediaIdJson.toString()));
+
+        JsonObject param = new JsonObject();
+        param.addProperty("buin", this.buin);
+        param.addProperty("appId", this.appId);
+        param.addProperty("encrypt", cipherId);
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse httpRsp = null;
+
+        String url = String.format("%s%s%s?accessToken=%s", YdApi.SCHEME, this.host, YdApi.API_DOWNLOAD_COMPLEX_MSG_ZIP, this.tokenClient.getToken());
+
+        try {
+            HttpPost httpPost = new HttpPost(url);
+            StringEntity entity = new StringEntity(param.toString(), ContentType.APPLICATION_JSON);
+            entity.setContentEncoding("UTF-8");
+            httpPost.setEntity(entity);
+
+            httpRsp = httpClient.execute(httpPost);
+            InputStream inputStream = httpRsp.getEntity().getContent();
+
+            byte[] encryptBuffer = Helper.readInputStream(inputStream);
+            String encryptContent = Helper.utf8String(encryptBuffer);
+            return this.crypto.decrypt(encryptContent);
+        } catch (IOException e) {
+            throw new HttpRequestException(0, e.getMessage(), e);
+        } finally {
+            Helper.close(httpRsp);
+            Helper.close(httpClient);
+        }
     }
 
 }
